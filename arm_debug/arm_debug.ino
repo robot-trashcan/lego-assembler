f32776191cb3756008b55163d936aa5d2144cc74
centipeda <joshua.c.guam@gmail.com>
2021-09-30 11:07:44 -0400

    add serial action

--- arm_debug/arm_debug.ino
+++ arm_debug/arm_debug.ino
@@ -15,6 +15,13 @@
 SoftwareSerial mySerial(rxPin, txPin);
 LobotServoController myse(mySerial);
 
+int toMove;
+int pos;
+
+int positions[] = {
+  1500, 1500, 1500, 1500, 1500, 1500
+};
+
 void reset_position() {
   myse.moveServos(6,2000,1,1500,2,1500,3,1500,4,1500,5,1500,6,1500);
   delay(2000);
@@ -26,56 +33,38 @@ void reset_position_noclaw() {
 }
 
 void crane_position() {
-  myse.moveServos(3, 2000, 3, 2450, 4, 1080, 5, 1100);
+  myse.moveServos(3, 2000, 3, 2500, 4, 900, 5, 1300);
   delay(2000);
 }
-
-void test() {
-  myse.moveServos(2, 1500, 5, 1500, 6, 1500);
-  delay(1500);
-  crane_position();
-  myse.moveServos(2, 1000, 2, 1800, 6, 1720);
-  delay(1000);
-  myse.moveServo(4, 850, 1000);
-  delay(1000);
-  myse.moveServo(1, 1500, 1000);
-}
-void twist(int x) {
+void test(int x) {
   myse.moveServos(2, 1000, 2, 1500+x, 6, 1500+x);
   delay(1000);
 }
 
 void close_claw() {
-  myse.moveServos(1, 1000, 1, 2500);
+  myse.moveServos(1, 1000, 1, 2400);
   delay(1000);
-}
-
-void wait() {
-  while(!Serial.available());
-  Serial.read();
 }
 
 void setup() {
   pinMode(13,OUTPUT);
   mySerial.begin(9600);  // opens software serial port, sets data rate to 9600 bps
   Serial.begin(9600);
-  digitalWrite(13,HIGH); 
-
-  // test(300);
-
-  // delay(500);
-
-  // close_claw();
-
-  // reset_position_noclaw();
 
   reset_position();
-  delay(3000);
-  crane_position();
-  twist(-200);
-  close_claw();
-  test(); 
 }
 
 void loop() {
+  while(!Serial.available());
+  toMove = Serial.parseInt();
+  pos = Serial.parseInt();
+  myse.moveServo(toMove, pos, 1000);
+  positions[toMove-1] = pos;
+  for(int i = 0; i < 6; i++) {
+    Serial.print(i+1);
+    Serial.print(": ");
+    Serial.print(positions[i]);
+    Serial.print(", ");
+  }
+  Serial.print("\n");
 }
